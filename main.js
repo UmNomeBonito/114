787523f606a2d3d38bb38aeef842c7ae8d44e684
UmNomeBonito <116125650+UmNomeBonito@users.noreply.github.com>
2024-08-22 19:46:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload() {
+    // Função deixada vazia para o próximo projeto
+}
+
+function setup() {
+    let canvas = createCanvas(400, 400);
+    canvas.parent('myCanvas');
+    canvas.center();
+}
+
+function draw() {
+    // Função deixada vazia para o próximo projeto
+}
+
+function onClick() {
+    saveCanvas(canvas, 'imagem-com-filtro', 'jpg');
+}
